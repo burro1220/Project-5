79fe5c60545c7ed8ad66e528345b91adb6f5a7fc
Blake Burroughs <b.burro@gmail.com>
2019-03-18 15:10:57 -0500

    working on search

--- js/script.js
+++ js/script.js
@@ -17,7 +17,31 @@ function getEmployees(){
         let cards = document.querySelectorAll('.card')
         cards.forEach((card, i) => {
             card.addEventListener('click', function(evt) {
-            makeModal(json);
+                makeModal(json.results[i]);
+                //
+                //next button
+                document.querySelector('#modal-next').addEventListener('click', function () {
+                    if (i === json.results.length - 1) {
+                        i = 0;
+                    } else {
+                        i = i+1;
+                    }
+                    setModalData(json.results[i]);
+                
+                    
+                });
+                //prev button
+                document.querySelector('#modal-prev').addEventListener('click', function () {
+                    if (i === 0) {
+                        i = cards.length - 1;
+                    }else {
+                        i = i-1;
+                    }
+                    setModalData(json.results[i]);
+                
+                    
+                });
+                     
         });
         
     })
@@ -47,19 +71,20 @@ function makeEmployeeCards(json) {
 };
     
 function makeModal(json) { 
+    console.log(json);
     let modalWindow = document.createElement('div');
                 modalWindow.classList.add('modal-container');
                 modalWindow.innerHTML = `
                 <div class="modal">
                     <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
                     <div class="modal-info-container">
-                        <img class="modal-img" src="${json.results[i].picture.large}" alt="${json.results[i].name.first} profile picture">
-                        <h3 id="name" class="modal-name cap">${json.results[i].name.first} ${json.results[i].name.last}</h3>
-                        <p class="modal-text">${json.results[i].email}</p>
-                        <p class="modal-text cap">${json.results[i].location.city}</p>
+                        <img class="modal-img" src="${json.picture.large}" alt="${json.name.first} profile picture">
+                        <h3 id="name" class="modal-name cap">${json.name.first} ${json.name.last}</h3>
+                        <p class="modal-text">${json.email}</p>
+                        <p class="modal-text cap">${json.location.city}</p>
                         <hr>
-                        <p class="modal-text">${json.results[i].cell}</p>
-                        <p class="modal-text">${json.results[i].location.street}, ${json.results[i].location.city}, ${json.results[i].location.state} ${json.results[i].location.postcode}</p>
+                        <p class="modal-text">${json.cell}</p>
+                        <p class="modal-text">${json.location.street}, ${json.location.city}, ${json.location.state} ${json.location.postcode}</p>
                     </div>
                 </div>
                 <div class="modal-btn-container">
@@ -73,49 +98,20 @@ function makeModal(json) {
                 document.querySelector('#modal-close-btn').addEventListener('click', function() {
                     document.querySelector('body').removeChild(modalWindow);
                 }); 
-                //
-                //next button
-                document.querySelector('#modal-next').addEventListener('click', function () {
-                    if (i === cards.length - 1) {
-                        i = 0;
-                    } else {
-                        i = i+1;
-                    }
-                    document.querySelector('.modal-info-container').innerHTML = `
-                        <img class="modal-img" src="${json.results[i].picture.large}" alt="${json.results[i].name.first} profile picture">
-                        <h3 id="name" class="modal-name cap">${json.results[i].name.first} ${json.results[i].name.last}</h3>
-                        <p class="modal-text">${json.results[i].email}</p>
-                        <p class="modal-text cap">${json.results[i].location.city}</p>
-                        <hr>
-                        <p class="modal-text">${json.results[i].cell}</p>
-                        <p class="modal-text">${json.results[i].location.street}, ${json.results[i].location.city}, ${json.results[i].location.state} ${json.results[i].location.postcode}</p>
-                   `;
                 
-                    
-                });
-                //prev button
-                document.querySelector('#modal-prev').addEventListener('click', function () {
-                    if (i === 0) {
-                        i = cards.length - 1;
-                    }else {
-                        i = i-1;
-                    }
-                    document.querySelector('.modal-info-container').innerHTML = `
-                        <img class="modal-img" src="${json.results[i].picture.large}" alt="${json.results[i].name.first} profile picture">
-                        <h3 id="name" class="modal-name cap">${json.results[i].name.first} ${json.results[i].name.last}</h3>
-                        <p class="modal-text">${json.results[i].email}</p>
-                        <p class="modal-text cap">${json.results[i].location.city}</p>
-                        <hr>
-                        <p class="modal-text">${json.results[i].cell}</p>
-                        <p class="modal-text">${json.results[i].location.street}, ${json.results[i].location.city}, ${json.results[i].location.state} ${json.results[i].location.postcode}</p>
-                   `;
-                
-                    
-                });
-                     
 };
     
-    
+function setModalData (json) {
+    document.querySelector('.modal-info-container').innerHTML = `
+        <img class="modal-img" src="${json.picture.large}" alt="${json.name.first} profile picture">
+        <h3 id="name" class="modal-name cap">${json.name.first} ${json.name.last}</h3>
+        <p class="modal-text">${json.email}</p>
+        <p class="modal-text cap">${json.location.city}</p>
+        <hr>
+        <p class="modal-text">${json.cell}</p>
+        <p class="modal-text">${json.location.street}, ${json.location.city}, ${json.location.state} ${json.location.postcode}</p>
+        `;
+};  
   
     
 
@@ -133,6 +129,7 @@ document.querySelector('.search-container').innerHTML = searchForm;
  function makeSearch (json) {
     //Search event listener
     document.querySelector('#search-input').addEventListener('keyup', function(e) {
+        console.log(json);
         let query = (e.target.value).toLowerCase();
         for (i=0; i < json.results.length; i++) {
             //grab name and show employee card if name matches query
@@ -141,7 +138,7 @@ document.querySelector('.search-container').innerHTML = searchForm;
             if(name.includes(query)){
                 console.log(`includes ${card}`);
             } else {
-                console.log(`query does not include ${name}`);
+                card.style.display = 'none';
             }
             
         }
